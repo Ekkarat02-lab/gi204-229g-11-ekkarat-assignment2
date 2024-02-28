bd524c2f93d812bfe8c87cc91989c00e5b526201
Ekkarat02-lab <ekkarat.thai@bumail.net>
2024-02-28 19:35:12 +0700

    assignment2

---
+++ Assets/Scripts/MoveRB.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MoveRB : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * moveSpeed;
+        rb.AddForce(movement);
+    }
+}
