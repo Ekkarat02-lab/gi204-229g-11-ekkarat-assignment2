ba1dda4c73be5a1fd49e9d28da85f3975759a2df
Ekkarat02-lab <ekkarat.thai@bumail.net>
2024-03-17 01:08:46 +0700

    Up

--- Assets/Scripts/AirPlane.cs
+++ Assets/Scripts/AirPlane.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AirPlane : MonoBehaviour
@@ -8,18 +5,7 @@ public class AirPlane : MonoBehaviour
     public Rigidbody rb;
 
     public float enginePowerThrust, liftBooster, drag, angularDrag;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.Space))
